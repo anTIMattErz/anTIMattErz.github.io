85f6d723ede7c55fc0110130c961e442cf30d00d
Michael Womack <mitwomack@gmail.com>
2024-03-30 17:26:33 -0700

    Adding navbar dynamic active styling

--- js/navbar_active.js
+++ js/navbar_active.js
@@ -1,3 +1,7 @@
+// referenced https://stackoverflow.com/questions/26819675/navbar-highlight-for-current-page?rq=3
+// and https://www.w3schools.com/js/js_whereto.asp
+
+// checks if current url matches the href of any a elements, then adds active class for css file to act on
 $(function(){
         $('a').each(function(){
             if ($(this).prop('href') == window.location.href) {
