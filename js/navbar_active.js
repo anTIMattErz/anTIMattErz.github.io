0902b2bffaebdf3b0b1404e3d7260cc4867b593c
Michael Womack <mitwomack@gmail.com>
2024-03-30 17:15:07 -0700

    Adding navbar dynamic active styling

--- js/navbar_active.js
+++ js/navbar_active.js
@@ -1,7 +1,7 @@
-function navbar_active() {
-    $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); $(this).parents('li').addClass('active');
-        }
+$(function(){
+        $('a').each(function(){
+            if ($(this).prop('href') == window.location.href) {
+                $(this).addClass('active'); $(this).parents('li').addClass('active');
+            }
+        });
     });
-};
